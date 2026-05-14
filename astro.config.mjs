302e469e9f9889aa99f2f35f4f4de1762f51c779
shahbazfareedchishti <shahbazfareedchishti@gmail.com>
2026-05-14 11:14:21 +0500

    Deploy to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://shahbazfdev.github.io',
   integrations: [tailwind()],
   image: {
     remotePatterns: [{ protocol: 'https', hostname: 'images.unsplash.com' }],
